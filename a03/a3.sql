45c064d396722b5e72ca3b28feb9b3d1934a361b
Chuang Li <chuang.li94@gmail.com>
2017-07-10 04:17:59 -0400

    asfsafasdf

--- a03/a3.sql
+++ a03/a3.sql
@@ -14,20 +14,20 @@ create table department ( \
 	primary key (dname), \
 	foreign key (chair) references instructor(id) )
 
---drop table student
+drop table student
 create table student ( \
 	name		varchar(20) not null, \
 	id			integer not null, \
 	major		varchar(20) not null, \
-	primary key (id),
+	primary key (id), \
 	foreign key (major) references department(dname) )
 
-----drop table course
+drop table course
 create table course ( \
 	name		varchar(20) not null, \
 	cnum		varchar(10) not null, \
-	dep	varchar(20) not null, \
-	primary key (cnum),
+	dep			varchar(20) not null, \
+	primary key (cnum), \
 	foreign key (dep) references department(dname) )
 
 drop table term
@@ -35,17 +35,41 @@ create table term ( \
 	id			varchar(10) not null, \
 	primary key (id) )
 
---drop table section
+drop table section
 create table section ( \
 	id			integer not null, \
 	cnum		varchar(10) not null, \
 	tid			varchar(10) not null, \
 	lecturer	integer not null, \
-	primary key (id, cnum, tid), 
+	primary key (id, cnum, tid), \
+	foreign key (cnum) references course(cnum), \
+	foreign key (tid) references term(tid), \
 	foreign key (lecturer) references instructor(id) )
 
 drop table offering
 create table offering ( \
 	cnum		varchar(10) not null, \
 	tid			varchar(10) not null, \ 
-	primary key (cnum, tid) )
+	primary key (cnum, tid), \
+	foreign key (cnum) references course(cnum), \
+	foreign key (tid) references term(tid) )
+
+drop table enrollment
+create table enrollment ( \
+	sid			integer not null, \
+	secid		integer not null, \
+	grade		integer not null, \
+	primary key (sid, secid), \
+	foreign key (sid) references student(id), \
+	foreign key (secid) references section(id) )
+
+drop table schedule
+create table schedule ( \
+	sid			integer not null, \
+	secid		integer not null, \
+	cnum 		varchar(10) not null, \
+	tid			varchar(10) not null, \
+	foreign key (sid) references student(id), \
+	foreign key (secid) references section(id), \
+	foreign key (cnum) references course(cnum), \
+	foreign key (tid) references term(tid) )
