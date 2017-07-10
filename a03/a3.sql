7be24c19ac6e7e97323d97d89d73894aad28944d
Chuang Li <chuang.li94@gmail.com>
2017-07-10 03:47:39 -0400

    asdfsdfa

--- a03/a3.sql
+++ a03/a3.sql
@@ -1,10 +1,20 @@
 connect to cs348
-
+/*
+drop table book
+drop table author
+drop table wrote
+drop table article
+drop table proceedings
+drop table journal
+drop table publication
+*/
 drop table department
 create table department ( \
 	dname		varchar(20) not null, \
 	location	varchar(20) not null, \
-	primary key (dname) )
+	chair		integer not null, \
+	primary key (dname), \
+	foreign key (chair) references instructor(id) )
 
 drop table instructor
 create table instructor ( \
@@ -16,13 +26,17 @@ drop table student
 create table student ( \
 	name		varchar(20) not null, \
 	id			integer not null, \
-	primary key (id) )
+	major		varchar(20) not null, \
+	primary key (id),
+	foreign key (major) references department(dname) )
 
 drop table course
 create table course ( \
 	name		varchar(20) not null, \
 	cnum		varchar(10) not null, \
-	primary key (cnum) )
+	dep	varchar(20) not null, \
+	primary key (cnum),
+	foreign key (dep) references department(dname) )
 
 drop table term
 create table term ( \
@@ -33,4 +47,13 @@ drop table section
 create table section ( \
 	id			integer not null, \
 	cnum		varchar(10) not null, \
-	tid			varchar(10), not null )
+	tid			varchar(10) not null, \
+	lecturer	integer not null, \
+	primary key (id, cnum, tid), 
+	foreign key (lecturer) references instructor(id) )
+
+drop table offering
+create table offering ( \
+	cnum		varchar(10) not null, \
+	tid			varchar(10) not null, \ 
+	primary key (cnum, tid) )
