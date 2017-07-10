e459767a4e4332790b0065514a941b5c32a45264
Chuang Li <chuang.li94@gmail.com>
2017-07-10 03:28:41 -0400

    asdfsdfa

--- a03/a3.sql
+++ a03/a3.sql
@@ -5,3 +5,32 @@ create table department ( \
 	dname		varchar(20) not null, \
 	location	varchar(20) not null, \
 	primary key (dname) )
+
+drop table instructor
+create table instructor ( \
+	name		varchar(20) not null, \
+	id			integer not null, \
+	primary key (id) )
+
+drop table student
+create table student ( \
+	name		varchar(20) not null, \
+	id			integer not null, \
+	primary key (id) )
+
+drop table course
+create table course ( \
+	name		varchar(20) not null, \
+	cnum		varchar(10) not null, \
+	primary key (cnum) )
+
+drop table term
+create table term ( \
+	id			varchar(10) not null, \
+	primary key (id) )
+
+drop table section
+create table section ( \
+	id			integer not null, \
+	cnum		varchar(10) not null, \
+	tid			varchar(10), not null )
